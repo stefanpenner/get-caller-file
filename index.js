567b07beb9bf36e837a97e37333c2109f9059414
Stefan Penner <stefan.penner@gmail.com>
2015-12-30 14:50:31 -0800

    allow position to be set

--- index.js
+++ index.js
@@ -5,14 +5,16 @@
 //
 // Inspired by http://stackoverflow.com/questions/13227489
 
-module.exports = function getCallerFile() {
+module.exports = function getCallerFile(_position) {
   var oldPrepareStackTrace = Error.prepareStackTrace;
   Error.prepareStackTrace = function(err, stack) { return stack; };
   var stack = new Error().stack;
   Error.prepareStackTrace = oldPrepareStackTrace;
 
+  var position = _position ? _position : 2;
+
   // stack[0] holds this file
   // stack[1] holds where this function was called
   // stack[2] holds the file we're interested in
-  return stack[2] ? stack[2].getFileName() : undefined;
+  return stack[position] ? stack[position].getFileName() : undefined;
 };
