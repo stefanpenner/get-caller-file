69482b5e9932704e1712dfbd98075e09a8cff480
Stefan Penner <stefan.penner@gmail.com>
2018-07-11 07:42:22 -0700

    Bump node engine support

--- index.js
+++ index.js
@@ -5,16 +5,15 @@
 //
 // Inspired by http://stackoverflow.com/questions/13227489
 
-module.exports = function getCallerFile(_position) {
-  var position = _position ? _position : 2;
+module.exports = function getCallerFile(position = 2) {
 
   if (position >= Error.stackTraceLimit) {
     throw new TypeError('getCallerFile(position) requires position be less then Error.stackTraceLimit but position was: `' + position + '` and Error.stackTraceLimit was: `' + Error.stackTraceLimit + '`');
   }
 
-  var oldPrepareStackTrace = Error.prepareStackTrace;
-  Error.prepareStackTrace = function(err, stack) { return stack; };
-  var stack = new Error().stack;
+  const oldPrepareStackTrace = Error.prepareStackTrace;
+  Error.prepareStackTrace = (err, stack)  => stack;
+  const stack = new Error().stack;
   Error.prepareStackTrace = oldPrepareStackTrace;
 
 
